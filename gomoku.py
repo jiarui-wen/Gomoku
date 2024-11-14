698be173f4d72a39970452fa1ae9e22a10eddf28
Jerry Jiang <jerryjiang06@gmail.com>
2024-11-14 12:19:04 -0500

    changed two numbers

--- gomoku.py
+++ gomoku.py
@@ -176,8 +176,8 @@ def is_win(board):
     return "Continue Playing"
 
 def colourwin(col, board):
-    for i in range(len(board)-5):
-        for j in range(len(board[0])-5):
+    for i in range(len(board)-4):
+        for j in range(len(board[0])-4):
             if board[i][j]==col and board[i][j+1]==col and board[i][j+2]==col and board[i][j+3]==col and board[i][j+4]==col:
                 return True
             if board[i][j]==col and board[i+1][j]==col and board[i+2][j]==col and board[i+3][j]==col and board[i+4][j]==col:
